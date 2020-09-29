01eaaccb3175858d5da0f429c1c126125c8a1f02
James Henager <jameschenager@gmail.com>
2020-09-29 16:13:36 -0700

    add count prop with increment and decrement to listitem
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- App.js
+++ App.js
@@ -52,4 +52,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 const ListItem = ({item, deleteItem}) => {
+  const [isDone, setIsDone] = useState(false);
+  const [counter, setCounter] = useState(1);
+
+  const styleOfItemIsDone = isDone ? styles.strike : null;
+  
+  const decreaseCounter = () => {
+    if(counter > 1) {
+      setCounter(counter - 1)
+    }
+  }
+
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
-        <Text style={styles.listItemText}>{item.text}</Text>
+        <Text style={styles.listItemText, styleOfItemIsDone}  onPress={() => setIsDone(!isDone)}>{item.text}</Text>
+        <View style={styles.count}>
+          <Icon style={{padding: 5}} name="minus" size={20} color="blue" onPress={decreaseCounter}/>
+          <Text>{counter}</Text>
+          <Icon style={{padding: 5}} name="plus" size={20} color="blue" onPress={() => setCounter(counter + 1)}/>
+        </View>
         <Icon name="remove" size={20} color="firebrick" onPress={() => deleteItem(item.id)} />
       </View>
     </TouchableOpacity>
@@ -26,7 +42,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',   
   },
   listItemText: {
-    fontSize: 18,
+    fontSize: 40,
+  },
+  strike: {
+    textDecorationLine: 'line-through',
+  },
+  count: {
+    padding: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   }
 });
 
